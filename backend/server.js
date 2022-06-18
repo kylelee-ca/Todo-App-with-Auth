a89587445390ac653b42faf2c38b4f4b19be999f
Kyle Lee <kyleleeca@gmail.com>
2022-06-18 14:23:17 -0400

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -12,6 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/todos', require('./routes/todoRoutes'));
 app.use('/api/users', require('./routes/userRoutes.js'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Not in production mode'))
+}
 
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
